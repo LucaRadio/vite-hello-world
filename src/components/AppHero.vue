22669a08f2a242b217ad5e4fa2ffa59080580cb4
Luca Radio <luca.1407@hotmail.it>
2022-12-01 15:00:52 +0100

    Complete Exercise with bonus

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -5,8 +5,6 @@ export default {
 </script>
 
 <template>
-    <section class="w-100 text-center">
-        <img src="https://www.bootstrapdash.com/wp-content/uploads/2020/01/BD-jumbotron-examples.jpg" alt="Jumbotron"
-            class="img-fluid">
+    <section>
     </section>
 </template>
